c862ea284985d1c5e0d6cbd77c799663fcaa91a5
niekas <albertasgim@gmail.com>
2020-08-18 22:13:54 +0300

    Redirect shortened URLs to original ones
    Also add <a> tag for short_url in index page for easier testing.

--- shortify/urls.py
+++ shortify/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 
 from shortify import views
 
 urlpatterns = [
-    path("", views.index),
+    path("", views.index, name="index"),
+    re_path("^(?P<short_path>\w+)/$", views.redirect_to_url, name="redirect_to_url"),
     path("admin/", admin.site.urls),
 ]
--- shortify/views.py
+++ shortify/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
 
 from shortify.forms import URLForm
+from shortify.models import ShortenedURL
 
 
 def index(request):
@@ -16,3 +18,8 @@ def index(request):
     else:
         form = URLForm()
     return render(request, "shortify/index.html", {"form": form})
+
+
+def redirect_to_url(request, short_path):
+    shortened_url = get_object_or_404(ShortenedURL, pk=short_path)
+    return HttpResponseRedirect(shortened_url.url)
